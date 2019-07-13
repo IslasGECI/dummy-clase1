3e60551c820268ce4e002cf2849b6302449b644a
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2019-07-12 23:10:40 -0700

    Regresa sólo el resultado

--- src/dummy_transformation.sh
+++ src/dummy_transformation.sh
@@ -2,10 +2,6 @@
 X=$1
 M=${2:-1}
 B=${3:-0}
-echo "Argumento: $X"
-echo "Escalamiento: $M"
-echo "Traslación: $B"
 # Y=$(( $M*$X+$B ))
 Y=$(add_offset.py $B $(scale_factor.R $M $X))
-echo "Resultado: $Y"
-return $Y
+echo "$Y"
